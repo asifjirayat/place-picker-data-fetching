8c3617ca1d13be97b2a0569f6462c85f30887b3f
Asif <asifjirayat@gmail.com>
2025-09-07 20:45:48 +0530

    Dummy backend with express

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import { useRef, useState, useCallback } from "react";
+import Places from "./components/Places.jsx";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import AvailablePlaces from "./components/AvailablePlaces.jsx";
+import logoImg from "./assets/logo.png";
 
 const App = () => {
-  return <div>App</div>;
+  const selectedPlace = useRef();
+  const [userPlaces, setUserPlaces] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const handleStartRemovePlace = (place) => {
+    setModalIsOpen(true);
+    selectedPlace.current = place;
+  };
+
+  const handleStopRemovePlace = () => {
+    setModalIsOpen(false);
+  };
+
+  const handleSelectPlace = (selectedPlace) => {
+    setUserPlaces((prevPickedPlaces) => {
+      if (!prevPickedPlaces) {
+        prevPickedPlaces = [];
+      }
+      if (prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
+        return prevPickedPlaces;
+      }
+      return [selectedPlace, ...prevPickedPlaces];
+    });
+  };
+
+  const handleRemovePlace = useCallback(async () => {
+    setUserPlaces((prevPickedPlaces) =>
+      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
+    );
+
+    setModalIsOpen(false);
+  }, []);
+
+  return (
+    <>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt="Stylized Globe" />
+        <h1>Place Picker</h1>
+        <p>
+          Create your personal collection of places you would like to visit or
+          you have visited.
+        </p>
+      </header>
+
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText="Select the places you would like to visit below."
+          places={userPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+      </main>
+
+      <AvailablePlaces onSelectPlace={handleSelectPlace} />
+    </>
+  );
 };
 
 export default App;
